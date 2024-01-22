40a9c9143dada746df0a6b3e83a40e7d662c0534
King <henrykelp@proton.me>
2024-01-21 22:27:34 -0500

    added non working slider

---
+++ data/slider.js
@@ -0,0 +1,9 @@
+//let slides = document.querySelectorAll('.slide');
+//let currentSlide = 0;
+//let slideInterval = setInterval(nextSlide, 3000);
+//
+//function nextSlide() {
+//  slides[currentSlide].className = 'slide';
+//  currentSlide = (currentSlide + 1) % slides.length;
+//  slides[currentSlide].className = 'slide active';
+//}
